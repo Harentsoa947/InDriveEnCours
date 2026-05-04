43c6061cd62db153a20fb9cb0b0485e90ff9f3b4
Jorris <jrjorris@gmail.com>
2026-05-04 20:19:36 +0300

    Ajustement register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,6 +8,41 @@
     <div class="container">
         <div class="box el1">
             <div class="row">
+                <div class="col-lg-6 im p-0">
+                    <img src="images/paul-hanaoka-D-qq7W751vs-unsplash.jpg" alt="">
+                </div>
+                <div class="col-lg-6 text">
+                    <div class="logo pe-4">
+                        <img src="images/indrive-icon-logo.png" alt="">
+                    </div>
+                    <div class="titre pe-4">
+                        <h1>Connectez-vous</h1>
+                        <p>Choisissez votre prix</p>
+                    </div>
+                    <form action="" method="post" class="pe-4">
+                        <input type="text" class="form-control" placeholder="Nom">
+                        <input type="password" class="form-control" placeholder="Mots de passe">
+                        <select name="" id="" class="form-control">
+                            <option value="" style="color: grey">---Passagier ou Chauffeur---</option>
+                            <option value="Passagier">Passagier</option>
+                            <option value="chauf">Chauffeur</option>
+                        </select>
+                        {{-- <div class="text-end mt-3">
+                            <a href="" style="text-decoration: none; color: #000;" class="ms-auto text-end">Etes vous chauffeur?</a>
+                        </div> --}}
+                        
+                        <input type="submit" value="Se connecter" class="btn btn-dark w-50 d-block mx-auto inscri my-4">
+                        <p class="insc">
+                            Pas encore de compte? 
+                            <a href="#" class="ins">S'inscrire</a>
+                        </p>
+                    </form>
+                </div>
+                
+            </div>
+        </div>
+        <div class="box el2">
+            <div class="row">
                 <div class="col-lg-6 text ">
                     <div class="logo px-4">
                         <img src="images/indrive-icon-logo.png" alt="">
@@ -51,41 +86,6 @@
                 <div class="col-lg-6 im d-none d-lg-block p-0">
                     <img src="images/paul-hanaoka-D-qq7W751vs-unsplash.jpg" alt="">
                 </div>
-            </div>
-        </div>
-        <div class="box el2">
-            <div class="row">
-                <div class="col-lg-6 im p-0">
-                    <img src="images/paul-hanaoka-D-qq7W751vs-unsplash.jpg" alt="">
-                </div>
-                <div class="col-lg-6 text">
-                    <div class="logo pe-4">
-                        <img src="images/indrive-icon-logo.png" alt="">
-                    </div>
-                    <div class="titre pe-4">
-                        <h1>Connectez-vous</h1>
-                        <p>Choisissez votre prix</p>
-                    </div>
-                    <form action="" method="post" class="pe-4">
-                        <input type="text" class="form-control" placeholder="Nom">
-                        <input type="password" class="form-control" placeholder="Mots de passe">
-                        <select name="" id="" class="form-control">
-                            <option value="" style="color: grey">---Passagier ou Chauffeur---</option>
-                            <option value="Passagier">Passagier</option>
-                            <option value="chauf">Chauffeur</option>
-                        </select>
-                        {{-- <div class="text-end mt-3">
-                            <a href="" style="text-decoration: none; color: #000;" class="ms-auto text-end">Etes vous chauffeur?</a>
-                        </div> --}}
-                        
-                        <input type="submit" value="Se connecter" class="btn btn-dark w-50 d-block mx-auto inscri my-4">
-                        <p class="insc">
-                            Pas encore de compte? 
-                            <a href="#" class="ins">S'inscrire</a>
-                        </p>
-                    </form>
-                </div>
-                
             </div>
         </div>
     </div>
